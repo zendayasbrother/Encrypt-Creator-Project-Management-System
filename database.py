fe1de74a81c4942bbb31555f6b1a5df1cf45104c
Daniel Onyeakazi <126322853+zendayasbrother@users.noreply.github.com>
2026-02-13 14:23:31 +0000

    Refactor database connection into DataManager class
    Refactored database connection logic into DataManager class and added methods for connecting and displaying tables.

--- database.py
+++ database.py
@@ -1,41 +1,39 @@
+import os
 import sqlite3
+import pandas as pd
+from dotenv import load_dotenv
 
-def connect_database():
-    try:
-        sqliteConnection = sqlite3.connect('EMPLOYEE_EMS.db')
-        cursor = sqliteConnection.cursor()
+load_dotenv()
 
-        # Integrity check
-        cursor.execute("PRAGMA integrity_check;")
-        if cursor.fetchone()[0] != "ok":
-            raise sqlite3.DatabaseError("Database integrity check failed.")
+class DataManager:
+    def __init__(self, db_path):
+        self.db_path = db_path
 
-        # Create Apprentices table
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS Employees (
-                ApprenticeID INTEGER PRIMARY KEY CHECK(EmploueeID >= 5001),
-                FirstName TEXT NOT NULL,
-                LastName TEXT NOT NULL,
-                Sex TEXT NOT NULL CHECK(Sex IN ('M', 'F')),
-                DOB TEXT,
-                Role TEXT NOT NULL
-            )
-        """)
+    def connect_database(self):
+        if not self.db_path or not os.path.exists(self.db_path):
+            print(f"Error: Database file not found at {self.db_path}")
+            return None, None
+            
+        try:
+            con = sqlite3.connect(self.db_path, uri =True)
+            cursor = con.cursor()
+            
+            # Security & Health Check
+            cursor.execute("PRAGMA integrity_check;")
+            if cursor.fetchone()[0] == "ok":
+                return con, cursor
+        except sqlite3.Error as error:
+            print(f"Database error: {error}")
+            return None, None
 
-        # Create Administrators table
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS Administrators (
-                username TEXT PRIMARY KEY,
-                password TEXT NOT NULL
-            )
-        """)
-
-        # Insert default admin if not exists
-        cursor.execute("INSERT OR IGNORE INTO Administrators (username, password) VALUES (?, ?)", ('zendayasbrother', '79022'))
-
-        sqliteConnection.commit()
-        return sqliteConnection, cursor
-
-    except sqlite3.Error as error:
-        print(f"Database error: {error}")
-        return None, None
+    def display_table(self, table_name):
+        conn, _ = self.connect_database()
+        if conn:
+            try:
+                df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
+                print(f"\n--- {table_name} Table ---")
+                print(df)
+            except Exception as e:
+                print(f"Error reading table: {e}")
+            finally:
+                conn.close()
